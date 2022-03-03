856b79be0f3293deab534589613797b9dbb45c37
pallomagualter <pallomagualter@gmail.com>
2022-03-03 10:32:10 -0300

    using eslint and prettier

---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export function CreateCourses(request: Request, response: Response) {
+
+}
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello World!" });
-});
-
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!"));
