96fff04ef45d35f20721df756a438e14a92876e2
pallomagualter <pallomagualter@gmail.com>
2022-03-03 17:52:42 -0300

    create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,15 +9,17 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
     created_at: new Date(),
-  };
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
