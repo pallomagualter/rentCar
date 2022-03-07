40b0180228ad3464d6ff218aec02f39ba0b41b50
pallomagualter <pallomagualter@gmail.com>
2022-03-07 09:26:46 -0300

    Princípio de Liskov

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,18 @@
 import { Router } from 'express';
+
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
-const categoriesRoutes = Router();
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
+const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists! " });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
